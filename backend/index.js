a4ffb44242974864a0ce4cb0d5038163937547cc
L O U I S <luis_henano@hotmail.com>
2025-01-08 16:13:42 -0300

    added port to .env

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import { MercadoPagoConfig, Preference } from 'mercadopago';
 import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT | 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
