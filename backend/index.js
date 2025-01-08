f2dfdc26c664f463f91a66ee83f5d29357471068
L O U I S <luis_henano@hotmail.com>
2025-01-07 22:25:57 -0300

    added cors and dotenv config

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('louie wuz here! 👽');
